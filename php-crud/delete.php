abc78132a52895fbe2888eb7bfbeed4d664931b0
Eko Sulistiyono <55071271+cedonulfi@users.noreply.github.com>
2024-11-28 00:00:24 +0700

    Update php-crud

---
+++ php-crud/delete.php
@@ -0,0 +1,11 @@
+<?php
+include 'connection.php';
+
+$id = $_GET['id'];
+$query = "DELETE FROM users WHERE id = $id";
+if ($conn->query($query)) {
+    header('Location: index.php');
+} else {
+    echo "Error: " . $conn->error;
+}
+?>
